d903b04f481a2dc45f7a8daeb9f74095816e38c6
tcolour <mizukit21@hotmail.com>
2023-06-11 22:18:40 +0900

    5章課題提出

---
+++ JavaStudy/5-1/src/main/Main.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<String>array= new ArrayList<String>();
+		array.add("hoge");
+		array.add("pos");
+		array.add("foo");
+		
+		array.set(1, "bar");
+		System.out.println("fooのインデックス:"+ array.indexOf("foo"));
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("address","///// ");
+		map.put("name","Kosuke");
+		map.put("age", 29);
+		
+		for(String key: map.keySet()) {
+			System.out.println("key:" + key);
+		}	
+		for(Object value: map.values()) {
+			System.out.println("value :" + value);
+		}
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH)+1;
+        int date = cal.get(Calendar.DATE);
+        
+        int[]arrayDate= new int[3];
+        arrayDate[0] = year;
+        arrayDate[1] = month;
+        arrayDate[2] = date;   
+        
+        List<int[]>list= Arrays.asList(arrayDate);
+        
+        for(int element: arrayDate){
+        	System.out.println(element);
+        }   
+		}
+}
+
+	
+
