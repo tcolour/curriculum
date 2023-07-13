4896dedc2c141e2dcdc1c26ab316344f4e5aae04
tcolour <mizukit21@hotmail.com>
2023-07-13 18:12:51 +0900

    第6章チェックテスト（確認用）

---
+++ JavaStudy/6-1/src/main/java/SelectServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SelectServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        String value = request.getParameter("month");
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("今は"+value+"です。");
+    }
+}
